cde45dda3a2a18e922186d82e533ee18218b2cb1
Prashant Rizal <prsnt.rizal@gmail.com>
2025-03-28 20:15:57 +1000

    ECAP-11 Get Items implemented

--- backend/controllers/itemController.js
+++ backend/controllers/itemController.js
@@ -3,9 +3,20 @@ const Task = require('../models/Items');
 const addItems = async (req , res ) => {
     const { title, description, deadline } = req.body;
     try {
-    const task = await Task.create({ userId: req.user.id, title, description, deadline });
-    res.status(201).json(task);
+        const task = await Task.create({ userId: req.user.id, title, description, deadline });
+        res.status(201).json(task);
     } catch (error) {
-    res.status(500).json({ message: error.message });
+        res.status(500).json({ message: error.message });
     }
     };
+
+    const getItems = async (req , res ) => {
+        try {
+            const tasks = await Task.find({ userId: req.user.id });
+            res.json(tasks);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    };
+
+    
